c7a3e5aac0ec8326bba4c4ca1dc3ec1540252fd7
p1nkrabb1t <pinkcyberbunny@gmail.com>
2018-12-10 19:14:52 +0000

    implement cursor loader

--- app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.pets;
 
+import android.app.LoaderManager;
 import android.content.ContentValues;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -20,10 +23,16 @@ import com.example.android.pets.data.PetContract.PetEntry;
 /**
  * Displays list of pets that were entered and stored in the app.
  */
-public class CatalogActivity extends AppCompatActivity {
+public class CatalogActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     //PetDbHelper is no longer used
     //private PetDbHelper mDbHelper;
+
+    private static final int PET_LOADER = 0;
+
+
+    // This is the Adapter being used to display the list's data
+    PetCursorAdapter mAdapter;
 
 
     @Override
@@ -41,6 +50,17 @@ public class CatalogActivity extends AppCompatActivity {
             }
         });
 
+        ListView displayView = (ListView) findViewById(R.id.list);
+        // Find and set empty view on the ListView, so that it only shows when the list has 0 items.
+        View emptyView = findViewById(R.id.empty_view);
+        displayView.setEmptyView(emptyView);
+
+        mAdapter = new PetCursorAdapter(this, null);
+
+        displayView.setAdapter(mAdapter);
+
+        //start the loader
+        getLoaderManager().initLoader(PET_LOADER, null, this);
 
         //mDbHelper = new PetDbHelper(this);  --no longer used
 
@@ -49,78 +69,81 @@ public class CatalogActivity extends AppCompatActivity {
         //displayDatabaseInfo();
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        displayDatabaseInfo();
-    }
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        displayDatabaseInfo();
+//    }
 
-    /**
-     * Temporary helper method to display information in the onscreen TextView about the state of
-     * the pets database.
-     */
-    private void displayDatabaseInfo() {
-        // To access our database, we instantiate our subclass of SQLiteOpenHelper
-        // and pass the context, which is the current activity.
-
-
-        // Create and/or open a database to read from it
-        //SQLiteDatabase db = mDbHelper.getReadableDatabase();
-
-        // Perform this raw SQL query "SELECT * FROM pets"
-        // to get a Cursor that contains all rows from the pets table.
-        //Cursor cursor = db.rawQuery("SELECT * FROM " + PetEntry.TABLE_NAME, null);
-
-
-        String[] projection = {
-                PetEntry._ID,
-                PetEntry.COLUMN_NAME,
-                PetEntry.COLUMN_BREED,
-                PetEntry.COLUMN_GENDER,
-                PetEntry.COLUMN_WEIGHT
-        };
-
-        Cursor cursor = getContentResolver().query(PetEntry.CONTENT_URI, projection, null, null,
-                null);
-
-        ListView displayView = (ListView) findViewById(R.id.list);
-
-        PetCursorAdapter adapter = new PetCursorAdapter(this, cursor);
-
-        displayView.setAdapter(adapter);
-
-
-//            // Figure out the index of each column
-//            int idColumnIndex = cursor.getColumnIndex(PetEntry._ID);
-//            int nameColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_NAME);
-//            int breedColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_BREED);
-//            int genderColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_GENDER);
-//            int weightColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_WEIGHT);
-//
-//            // Iterate through all the returned rows in the cursor
-//            while (cursor.moveToNext()) {
-//                // Use that index to extract the String or Int value of the word
-//                // at the current row the cursor is on.
-//                int currentID = cursor.getInt(idColumnIndex);
-//                String currentName = cursor.getString(nameColumnIndex);
-//                String currentBreed = cursor.getString(breedColumnIndex);
-//                int currentGender = cursor.getInt(genderColumnIndex);
-//                int currentWeight = cursor.getInt(weightColumnIndex);
-//                // Display the values from each column of the current row in the cursor in the TextView
-//                displayView.append(("\n" + currentID + " - " +
-//                        currentName + " - " +
-//                        currentBreed + " - " +
-//                        currentGender + " - " +
-//                        currentWeight));
-//            }
+//    /**
+//     * Temporary helper method to display information in the onscreen TextView about the state of
+//     * the pets database.
+//     */
+//    private void displayDatabaseInfo() {
+//        // To access our database, we instantiate our subclass of SQLiteOpenHelper
+//        // and pass the context, which is the current activity.
 //
 //
-//        } finally {
-//            // Always close the cursor when you're done reading from it. This releases all its
-//            // resources and makes it invalid.
-//            cursor.close();
-//        }
-    }
+//        // Create and/or open a database to read from it
+//        //SQLiteDatabase db = mDbHelper.getReadableDatabase();
+//
+//        // Perform this raw SQL query "SELECT * FROM pets"
+//        // to get a Cursor that contains all rows from the pets table.
+//        //Cursor cursor = db.rawQuery("SELECT * FROM " + PetEntry.TABLE_NAME, null);
+//
+//
+//        String[] projection = {
+//                PetEntry._ID,
+//                PetEntry.COLUMN_NAME,
+//                PetEntry.COLUMN_BREED,
+//                PetEntry.COLUMN_GENDER,
+//                PetEntry.COLUMN_WEIGHT
+//        };
+//
+//        Cursor cursor = getContentResolver().query(PetEntry.CONTENT_URI, projection, null, null,
+//                null);
+//
+//        ListView displayView = (ListView) findViewById(R.id.list);
+//        // Find and set empty view on the ListView, so that it only shows when the list has 0 items.
+//        View emptyView = findViewById(R.id.empty_view);
+//        displayView.setEmptyView(emptyView);
+//
+//        PetCursorAdapter adapter = new PetCursorAdapter(this, cursor);
+//
+//        displayView.setAdapter(adapter);
+//
+//
+////            // Figure out the index of each column
+////            int idColumnIndex = cursor.getColumnIndex(PetEntry._ID);
+////            int nameColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_NAME);
+////            int breedColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_BREED);
+////            int genderColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_GENDER);
+////            int weightColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_WEIGHT);
+////
+////            // Iterate through all the returned rows in the cursor
+////            while (cursor.moveToNext()) {
+////                // Use that index to extract the String or Int value of the word
+////                // at the current row the cursor is on.
+////                int currentID = cursor.getInt(idColumnIndex);
+////                String currentName = cursor.getString(nameColumnIndex);
+////                String currentBreed = cursor.getString(breedColumnIndex);
+////                int currentGender = cursor.getInt(genderColumnIndex);
+////                int currentWeight = cursor.getInt(weightColumnIndex);
+////                // Display the values from each column of the current row in the cursor in the TextView
+////                displayView.append(("\n" + currentID + " - " +
+////                        currentName + " - " +
+////                        currentBreed + " - " +
+////                        currentGender + " - " +
+////                        currentWeight));
+////            }
+////
+////
+////        } finally {
+////            // Always close the cursor when you're done reading from it. This releases all its
+////            // resources and makes it invalid.
+////            cursor.close();
+////        }
+//    }
 
     private void insertPet() {
         //SQLiteDatabase db = mDbHelper.getWritableDatabase(); - no longer referenced directly
@@ -154,7 +177,7 @@ public class CatalogActivity extends AppCompatActivity {
             // Respond to a click on the "Insert dummy data" menu option
             case R.id.action_insert_dummy_data:
                 insertPet();
-                displayDatabaseInfo();
+                //displayDatabaseInfo();
                 return true;
             // Respond to a click on the "Delete all entries" menu option
             case R.id.action_delete_all_entries:
@@ -163,4 +186,22 @@ public class CatalogActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String[] Projection = new String[]{PetEntry._ID,
+                PetEntry.COLUMN_NAME,
+                PetEntry.COLUMN_BREED};
+        return new CursorLoader(this, PetEntry.CONTENT_URI, Projection, null, null, null);
     }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        mAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mAdapter.swapCursor(null);
+    }
+}
